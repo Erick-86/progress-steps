376abdae48052f143a3a78da3dfa0af9335a7e4c
FreeMan <zelonaire@gmail.com>
2023-03-04 12:04:01 +0000

    creating variables for elemnts

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//CREATING VARIABLES
+const progressBG = document.getElementById("progress-bg");
+const circles = document.querySelectorAll(".circles");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
