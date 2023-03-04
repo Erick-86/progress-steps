0c066c3cb2a6f0a27a0206f4a5b271ce10f9eb67
FreeMan <zelonaire@gmail.com>
2023-03-04 13:29:15 +0000

    update functionality

--- script.js
+++ script.js
@@ -17,14 +17,48 @@ next.addEventListener("click", () => {
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
+
+  update();
 });
 
 //PREV BTN
 prev.addEventListener("click", () => {
-    //Check to not let the current active go below 1
-    currentActive--
-    
-    if (currentActive < 1) {
-        currentActive = 1
-    }
+  //Check to not let the current active go below 1
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
 });
+
+//UPDATE FUNCTIONALITY
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  //When ever the next btn is clicked, active class is added to the circles
+  const actives = document.querySelectorAll(".active");
+  console.log(actives);
+
+  // Setting the width styling
+  progressBG.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + `%`;
+
+  //Disabling the next btn when the current active is equal to the circles.length
+  //Enabling the prev btn when the next btn is clicked
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
