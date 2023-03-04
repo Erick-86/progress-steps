34f83b97aa373fb593ea6620680a0a97d90f41af
FreeMan <zelonaire@gmail.com>
2023-03-04 12:07:28 +0000

    next btn functionality

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const progressBG = document.getElementById("progress-bg");
 const circles = document.querySelectorAll(".circles");
 const next = document.getElementById("next");
 const prev = document.getElementById("prev");
+
+let currentActive = 1;
+
+//Adding an EventListener and a function for the circles
+//NEXT BTN
+next.addEventListener("click", () => {
+  currentActive++;
+
+  //Cohecking to make the currentActive not go past the number of circles
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+});
